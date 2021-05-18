6f9615325fbaf5a329a6d9fa345134f6ab85a549
Anika <anika.jain@sap.com>
2021-05-18 18:06:03 +0530

    csjhd

--- scripts/fetch_gae_sdk.py
+++ scripts/fetch_gae_sdk.py
@@ -59,39 +59,4 @@ def get_sdk_urls(sdk_versions):
 
 
 def main(argv):
-    if len(argv) > 2:
-        print('Usage: {} [<destination_dir>]'.format(argv[0]))
-        return 1
-    dest_dir = argv[1] if len(argv) > 1 else '.'
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-
-    if os.path.exists(os.path.join(dest_dir, 'google_appengine')):
-        print('GAE SDK already installed at {}, exiting.'.format(dest_dir))
-        return 0
-
-    sdk_versions = get_gae_versions()
-    if not sdk_versions:
-        print('Error fetching GAE SDK version info')
-        return 1
-    sdk_urls = get_sdk_urls(sdk_versions)
-    for sdk_url in sdk_urls:
-        try:
-            sdk_contents = io.StringIO(urlopen(sdk_url).read())
-            break
-        except:
-            pass
-    else:
-        print('Could not read SDK from any of {}'.format(sdk_urls))
-        return 1
-    sdk_contents.seek(0)
-    try:
-        zip_contents = zipfile.ZipFile(sdk_contents)
-        zip_contents.extractall(dest_dir)
-        print('GAE SDK Installed to {}.'.format(dest_dir))
-    except:
-        print('Error extracting SDK contents')
-        return 1
-print('Error extracting SDK contents')
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[:]))
+   print("In fetch")
